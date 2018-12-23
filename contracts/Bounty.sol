fc8156a5d78f4fbaa7996794597d97e87574490e
Alex <imalex@pop-os.localdomain>
2018-12-22 17:43:11 -0800

    Add Bounty contract

---
+++ contracts/Bounty.sol
@@ -0,0 +1,142 @@
+pragma solidity ^ 0.5 .0;
+
+import 'installed_contracts/zeppelin/contracts/math/SafeMath.sol';
+
+contract Bounty is SafeMath {
+    using SafeMath for uint;
+    
+    address public owner;
+    uint public questionCount;
+    uint public answerCount;
+
+    constructor() payable public {
+        require(msg.value != 0, "Send some money");
+        owner = msg.sender;
+        questionCount = 0;
+        answerCount = 0;
+    }
+
+    mapping(uint => Question) public allQuestions;
+    mapping(uint => Answer) public allAnswers;
+
+    struct Question {
+        uint id;
+        string heading;
+        string description;
+        uint submitDate;
+        uint bountyAmount;
+        address payable funder;
+        address winner;
+    }
+
+    struct Answer {
+        uint id;
+        uint questionId;
+        string description;
+        uint submitDate;
+        bool accepted;
+        bool rejected;
+        address payable proposer;
+    }
+
+    function addQuestion(string memory _heading, string memory _description)
+    public
+    payable
+    returns(uint id) {
+        require(msg.value != 0, "Add a bounty amount");
+        
+        allQuestions[questionCount] = Question({
+            id: questionCount,
+            heading: _heading,
+            description: _description,
+            submitDate: now,
+            bountyAmount: msg.value,
+            funder: msg.sender,
+            winner: address(0)
+        });
+        questionCount += 1;
+        return id;
+    }
+
+    function addAnswer(uint _questionId, string memory _description) public returns(uint id) {
+        allAnswers[answerCount] = Answer({
+            id: answerCount,
+            questionId: _questionId,
+            description: _description,
+            submitDate: now,
+            accepted: false,
+            rejected: false,
+            proposer: msg.sender
+        });
+        answerCount += 1;
+        return id;
+    }
+
+    function getQuestion(uint _id) public view returns(
+        uint id,
+        string memory heading,
+        string memory description,
+        uint submitDate,
+        uint bountyAmount,
+        address funder,
+        address winner) {
+        id = allQuestions[_id].id;
+        heading = allQuestions[_id].heading;
+        description = allQuestions[_id].description;
+        bountyAmount = allQuestions[_id].bountyAmount;
+        funder = allQuestions[_id].funder;
+        winner = allQuestions[_id].winner;
+        return (id, heading, description, submitDate, bountyAmount, owner, winner);
+    }
+
+    function getAnswer(uint _id) public view returns(
+        uint id,
+        uint questionId,
+        string memory description,
+        uint submitDate,
+        bool accepted,
+        bool rejected,
+        address proposer
+    ) {
+        id = allAnswers[_id].id;
+        questionId = allAnswers[_id].questionId;
+        description = allAnswers[_id].description;
+        submitDate = allAnswers[_id].submitDate;
+        accepted = allAnswers[_id].accepted;
+        rejected = allAnswers[_id].rejected;
+        proposer = allAnswers[_id].proposer;
+
+        return (id, questionId, description, submitDate, accepted, rejected, owner);
+    }
+
+    function acceptAnswer(uint _id) public returns(
+        uint id) {
+        
+        require(msg.sender == allQuestions[allAnswers[_id].questionId].funder, "Not question funder");    
+        
+        // flip answer to true
+        allAnswers[_id].accepted = true;
+
+        // get question bounty amount
+        uint bountyAmount = allQuestions[allAnswers[_id].questionId].bountyAmount;
+
+        // record winner in question
+        allQuestions[allAnswers[_id].questionId].winner = allAnswers[_id].proposer;
+
+        // award bounty to proposer
+        allQuestions[allAnswers[_id].questionId].bountyAmount = 0;
+        address(allAnswers[_id].proposer).transfer(bountyAmount);
+
+        return id;
+    }
+
+    function rejectAnswer(uint _id) public returns(bool) {
+        allAnswers[_id].rejected = true;
+        return true;
+    }
+
+    function getContractBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+
+}
