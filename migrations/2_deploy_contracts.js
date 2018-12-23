7a961c20d99f364a17eecc50ccd2e0dd6cd1bb54
Alex <imalex@pop-os.localdomain>
2018-12-22 18:18:56 -0800

    Update migration script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var Bounty = artifacts.require('./Bounty.sol');
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+  deployer.deploy(Bounty);
 };
