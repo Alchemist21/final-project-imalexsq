30c1d7282f7ffb67177a29e4e709641839cfc566
Alex <imalex@pop-os.localdomain>
2018-12-27 11:37:09 -0800

    Add ropsten for deployment

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+var HDWalletProvider = require('truffle-hdwallet-provider');
+var mnemonic =
+  'famous call liquid cool paddle exist impose sadness wear tackle portion during rough bag erosion';
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -9,6 +12,15 @@ module.exports = {
       host: '127.0.0.1',
       port: 7545,
       network_id: '*' // Match any network id
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(
+          mnemonic,
+          'https://ropsten.infura.io/adf80880c9074524a08e44291a78f9e4'
+        );
+      },
+      network_id: 3
     }
   },
   contracts_build_directory: path.join(__dirname, 'client/src/contracts')
